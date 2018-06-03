118d04abf42de7e0978012ab5296e1e37c9bec1c
wfl19890917 <1126865919@qq.com>
2018-06-03 17:15:37 +0800

    event_manage提交

--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -9,9 +10,14 @@ def login_action(request):
         username=request.POST.get("username","")
         password=request.POST.get("password","")
         if username=="admin" and password=="admin123":
-            return HttpResponse('Login Success!')
+            response= HttpResponseRedirect("/event_manage/")
+            response.set_cookie("user",username,3600)
+            return response  
         else:
             return render(request,"index.html",{"error":"username or password error!"})
+def event_manage(request):
+    username=request.COOKIES.get("user","")
+    return render(request,"event_manage.html",{"user":username})
     
 
     
